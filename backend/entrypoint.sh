ec1e107523058e69ff293d0cf7d5c72b0be43654
alexrashkin <r100aa@yandex.ru>
2023-12-30 18:46:30 +0300

    new commit

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#python manage.py flush --no-input
+python manage.py flush --no-input
 
 python manage.py makemigrations
 
@@ -9,8 +9,8 @@ python manage.py migrate
 python manage.py load_materials
 python manage.py load_tags
 
-#gunicorn lina.wsgi:application --bind 0.0.0.0:80
+gunicorn lina.wsgi:application --bind 0.0.0.0:80
 
-#cp -r /app/collected_static/. /backend_static/static/
+cp -r /app/collected_static/. /backend_static/static/
 
-#source load_env.sh
+source load_env.sh
