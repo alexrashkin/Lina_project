723dc55a69af3577564280141b3683b09fd7e6c4
alexrashkin <r100aa@yandex.ru>
2024-01-12 14:56:00 +0300

    add Telegram link to footer

--- frontend/src/components/footer/index.js
+++ frontend/src/components/footer/index.js
@@ -1,15 +1,37 @@
-import styles from './style.module.css'
-import { Container, LinkComponent } from '../index'
+import React, { useEffect } from 'react';
+import { Container, Link } from 'react-router-dom'; // Импортируйте Link из react-router-dom
+
+import styles from './style.module.css';
+import telegramLogo from '../../pages/artist/telegram.png';
 
 const Footer = () => {
-  return <footer className={styles.footer}>
+  useEffect(() => {
+    const isMobile = window.matchMedia('only screen and (max-width: 600px)').matches;
+    const telegramLink = document.getElementById('telegramLink');
+
+    if (isMobile) {
+      telegramLink.href = 'https://t.me/angelinasvs777';
+    } else {
+      telegramLink.href = 'https://web.telegram.org/k/#@angelinasvs777';
+    }
+  }, []);
+
+  return (
+    <footer className={styles.footer}>
       <Container className={styles.footer__container}>
-        <LinkComponent href='#' title='Художник Ангелина Хижняк' className={styles.footer__brand} />
+        <Link to="#" title="Художник Ангелина Хижняк" className={styles.footer__brand} />
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <img src={telegramLogo} alt="Лого Телеграм" style={{ marginRight: '5px' }} />
+          <a id="telegramLink" target="_blank" rel="noopener noreferrer">
+            Телеграм
+          </a>
+        </div>
         <p className={styles.footer__brand}>
           Все права защищены © {new Date().getFullYear()}
         </p>
       </Container>
-  </footer>
-}
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
