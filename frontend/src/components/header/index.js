726a2ce83fcfe27033228d2f0aaaf4de0141c6d7
alexrashkin <r100aa@yandex.ru>
2024-01-13 18:24:30 +0300

    edit header design

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -1,41 +1,18 @@
-import { Particles } from 'react-tsparticles';
-
-import styles from './style.module.css';
-import { Nav, AccountMenu } from '../index.js';
-import Container from '../container';
+import styles from './style.module.css'
+import { Nav, AccountMenu } from '../index.js'
+import Container from '../container'
 
 const Header = ({ loggedIn, isSuperuser, onSignOut, orders }) => {
-  console.log('Rendering Header with Particles');
-  return (
-    <header className={styles.header}>
-      <Container>
-        <div className={styles.headerContent}>
-          <Nav loggedIn={loggedIn} orders={orders} isSuperuser={isSuperuser} />
-          <AccountMenu onSignOut={onSignOut} />
-        </div>
-        <Particles
-          params={{
-            particles: {
-              number: {
-                value: 80,
-              },
-              size: {
-                value: 3,
-              },
-            },
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: 'repulse',
-                },
-              },
-            },
-          }}
-        />
-      </Container>
-    </header>
-  );
-};
+  return <header
+    className={styles.header}
+  >
+    <Container>
+      <div className={styles.headerContent}>
+        <Nav loggedIn={loggedIn} orders={orders} isSuperuser={isSuperuser} />
+        <AccountMenu onSignOut={onSignOut} />
+      </div>
+    </Container>
+  </header>
+}
 
 export default Header
