75c58e8a3cb3268c3c5bf3965c7456261b01b34c
alexrashkin <r100aa@yandex.ru>
2024-01-13 22:31:40 +0300

    final commit

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ValidationError
 def validate_username(value):
     if re.search(r'^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$', value) is None:
         raise ValidationError(
-            (f'Не допустимые символы <{value}> в никенейм.'),
+            (f'Недопустимые символы <{value}> в никнейм.'),
             params={'value': value},
         )
     if value == 'me':
