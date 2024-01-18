d7caa404c4d732d130240a5c90299587d68c87b4
alexrashkin <r100aa@yandex.ru>
2024-01-18 23:39:46 +0300

    delete swagger

--- backend/lina/urls.py
+++ backend/lina/urls.py
@@ -2,29 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="SIP Service API",
-        default_version='v1',
-        description="Api to multiple parallel calls with recording",
-        contact=openapi.Contact(email="r100aa@yandex.ru"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls', namespace='api')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
-         name='schema-redoc'),
 ]
 
 if settings.DEBUG:
