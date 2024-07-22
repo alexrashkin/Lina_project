510f7c1aa284b916d85a6ef56837a906bbe73be9
alexrashkin <r100aa@yandex.ru>
2024-07-22 18:35:46 +0300

    new design

--- frontend/src/pages/artist/index.js
+++ frontend/src/pages/artist/index.js
@@ -5,28 +5,32 @@ import artist_photo from '../../pages/artist/artist.jpg';
 import telegram_logo from '../../pages/artist/telegram.png';
 
 const Artist = () => {
-  const headingStyle = {
-    color: 'rgb(232, 240, 201)',
-    marginBottom: '20px',
-  };
-
-  const sectionStyle = {
-    marginBottom: '60px',
-  };
-
-  const imageStyle = {
-    maxWidth: '40%',
-    height: 'auto',
-    borderRadius: '8px',
-    marginRight: '20px',
-  };
-
-  const contentStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'left',
-    marginBottom: '40px',
+  const styles = {
+    heading: {
+      color: 'rgb(232, 240, 201)',
+      marginBottom: '20px',
+    },
+    section: {
+      marginBottom: '60px',
+    },
+    image: {
+      maxWidth: '40%',
+      height: 'auto',
+      borderRadius: '8px',
+      marginRight: '20px',
+    },
+    content: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      textAlign: 'left',
+      marginBottom: '40px',
+    },
+    mobileContent: {
+      flexDirection: 'column', // Размещение элементов в колонку для мобильной версии
+      alignItems: 'center', // Центрирование элементов
+      textAlign: 'center', // Центрирование текста
+    }
   };
 
   useEffect(() => {
@@ -43,28 +47,28 @@ const Artist = () => {
   return (
     <Main>
       <Container>
-        <div style={contentStyle}>
-          <img src={artist_photo} alt="Фото художника" style={imageStyle} />
+        <div style={{...styles.content, ...(window.innerWidth <= 600 ? styles.mobileContent : {})}}>
+          <img src={artist_photo} alt="Фото художника" style={styles.image} />
           <div>
-            <h2 style={headingStyle}>Ангелина Игоревна Хижняк - профессиональный художник</h2>
+            <h2 style={styles.heading}>Ангелина Игоревна Хижняк - профессиональный художник</h2>
             <p>Здравствуйте! Добро пожаловать на мой сайт-портфолио. Здесь Вы можете ознакомиться с информацией обо мне и увидеть мои работы. Приятного просмотра!</p>
           </div>
         </div>
-        <div style={{ ...sectionStyle, marginBottom: '40px', marginTop: '35px' }}>
+        <div style={{ ...styles.section, marginBottom: '40px', marginTop: '35px' }}>
           <strong>Коротко обо мне:</strong>
           <p>Мой опыт работы составляет более 10 лет.</p>
           <p>Работаю в разных стилях: от простой абстракции до гиперреализма. В работе использую технику аэрографии.</p>
         </div>
         <p>Пишу неповторимые картины любого размера под Ваш интерьер, в единственном экземпляре. А так же, с удовольствием выполню другую индивидуальную работу по Вашему заказу (например, художественная роспись стен, мебели, предметов интерьера).</p>
         <p>В работе использую только качественные материалы.</p>
-        <div style={{ ...sectionStyle, marginBottom: '40px', marginTop: '35px' }}>
+        <div style={{ ...styles.section, marginBottom: '40px', marginTop: '35px' }}>
           <p>Все представленные на сайте работы выполнены мной и находятся в частных коллекциях как в России, так и зарубежом.</p>
           <strong>Образование:</strong>
           <p>Художественная школа г. Ставрополь (2003 - 2013 гг.)</p>
           <p>Санкт-Петербургский государственный университет промышленных технологий и дизайна (2013 - 2019 гг.)</p>
           <p>Первый Центр Аэрографии г. Санкт-Петербург (2016 - 2017 гг.)</p>
         </div>
-        <div style={sectionStyle}>
+        <div style={styles.section}>
           <strong>Контакты:</strong>
           <p><strong>Оформить индивидуальный заказ, задать вопрос или оставить отзыв на работу можно тут:</strong></p>
           <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
