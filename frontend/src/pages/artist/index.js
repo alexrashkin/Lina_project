c3c8419ac21a9a2fd5c809bd234883154f70727d
alexrashkin <r100aa@yandex.ru>
2024-07-24 00:58:24 +0300

    new design

--- frontend/src/pages/artist/index.js
+++ frontend/src/pages/artist/index.js
@@ -56,7 +56,7 @@ const Artist = () => {
               <p><strong>Оформить индивидуальный заказ, задать вопрос или оставить отзыв на работу можно тут:</strong></p>
             <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
               <a style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'blue' }} id="telegramLink" target="_blank" rel="noopener noreferrer">
-                <img src={telegram_logo} alt="Лого Телеграм" style={{ marginRight: '5px' }} />
+              <img src={telegram_logo} alt="Лого Телеграм" style={{ marginRight: '5px', marginBottom: '22px' }} />
                 <p>Телеграм</p>
               </a>
             </div>
