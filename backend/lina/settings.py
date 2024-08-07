9e2ab85c4898d60b8a374f3d6e52ba35cfece4f1
alexrashkin <r100aa@yandex.ru>
2024-08-07 19:49:29 +0300

    switch off limit

--- backend/lina/settings.py
+++ backend/lina/settings.py
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6,
+    'PAGE_SIZE': 1000,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ]
